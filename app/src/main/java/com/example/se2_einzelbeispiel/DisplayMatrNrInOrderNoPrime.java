75f30c429a089fa7ed4e7b9cc1cc457c9589c180
Sarah Pschernig <sapschernig@edu.aau.at>
2021-03-12 21:50:38 +0100

    added second activity

---
+++ app/src/main/java/com/example/se2_einzelbeispiel/DisplayMatrNrInOrderNoPrime.java
@@ -0,0 +1,14 @@
+package com.example.se2_einzelbeispiel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayMatrNrInOrderNoPrime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_matr_nr_in_order_no_prime);
+    }
+}
