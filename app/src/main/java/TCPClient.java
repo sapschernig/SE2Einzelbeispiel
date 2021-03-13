51f3bd11c1d165e3a8aec14e19d06565773a08f3
Sarah Pschernig <sapschernig@edu.aau.at>
2021-03-13 21:30:55 +0100

    added class @TCPClient

---
+++ app/src/main/java/TCPClient.java
@@ -0,0 +1,32 @@
+import android.provider.ContactsContract;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TCPClient {
+
+    public static void main(String[] args) throws Exception {
+        String sentence;
+        String modifiedSentence;
+
+        BufferedReader inFromUser= new BufferedReader(new InputStreamReader(System.in));
+
+        Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+        DataOutputStream outToServer= new DataOutputStream(clientSocket.getOutputStream());
+
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        sentence=inFromUser.readLine();
+
+        outToServer.writeBytes(sentence+'\n');
+
+        modifiedSentence=inFromServer.readLine();
+
+        System.out.println("Antwort vom Server: "+ modifiedSentence);
+
+        clientSocket.close();
+    }
+}
--- app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
@@ -22,4 +22,8 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+    public void ConnectToServer(){
+
+
+    }
 }
